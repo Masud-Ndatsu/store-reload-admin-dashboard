cdf5cc6efecbe91a4fa50106280dcecc1da43e03
Masud-Ndatsu <masudndatsu@gmail.com>
2023-08-08 01:56:37 +0100

    feat: updated, modified and made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import {} from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  redirect,
+} from "react-router-dom";
 import { Signin } from "./pages/signin";
 import { Signup } from "./pages/signup";
 import { ForgotPassword } from "./pages/forgotpassword";
 import { ResetPassword } from "./pages/resetpassword";
 import { NewPassword } from "./pages/newpassword";
 import { Dashboard } from "./pages/dashboard";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/dashboard", element: <Dashboard /> },
-  { path: "/auth/signin", element: <Signin /> },
-  { path: "/auth/signup", element: <Signup /> },
-  { path: "/auth/forgot-password", element: <ForgotPassword /> },
-  { path: "/auth/reset-password", element: <ResetPassword /> },
-  { path: "/auth/change-password", element: <NewPassword /> },
-]);
+import { useAuthToken } from "./hooks/useAuthToken";
+redirect("");
 
 function App() {
+  const isAuthenticated = useAuthToken();
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              !isAuthenticated.user ? (
+                <Navigate to="/auth/signin" />
+              ) : (
+                <Dashboard />
+              )
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              !isAuthenticated.user ? (
+                <Navigate to="/auth/signin" />
+              ) : (
+                <Dashboard />
+              )
+            }
+          />
+          <Route path="auth/signin" element={<Signin />} />
+          <Route path="auth/signup" element={<Signup />} />
+          <Route path="auth/forgot-password" element={<ForgotPassword />} />
+          <Route path="auth/reset-password" element={<ResetPassword />} />
+          <Route path="auth/change-password" element={<NewPassword />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/utils/auth.ts
@@ -0,0 +1,5 @@
+import { ICustomer } from "../components/CustomersInfo/types";
+
+const isAuthenticated = (): ICustomer | void => {
+  return;
+};
