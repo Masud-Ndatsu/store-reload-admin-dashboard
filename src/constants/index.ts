7557ce975108ee973f305c578d3d4ff6de8bba7e
Masud-Ndatsu <masudndatsu@gmail.com>
2023-09-21 01:06:40 +0100

    feat: Made new changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,11 @@
-// export const USER_URL = "https://store-reload.onrender.com/api/v1/admin/user";
-export const USER_URL = "http://localhost:5000/api/v1/admin/user";
-// export const AUTH_URL = "https://store-reload.onrender.com/api/v1/admin/auth";
-export const AUTH_URL = "http://localhost:5000/api/v1/admin/auth";
+export const USER_URL = "https://store-reload.onrender.com/api/v1/admin/user";
+// export const USER_URL = "http://localhost:5000/api/v1/admin/user";
+export const AUTH_URL = "https://store-reload.onrender.com/api/v1/admin/auth";
+// export const AUTH_URL = "http://localhost:5000/api/v1/admin/auth";
 
-// export const PRODUCT_URL =
-//   "https://store-reload.onrender.com/api/v1/admin/product";
-export const PRODUCT_URL = "http://localhost:5000/api/v1/admin/product";
+export const PRODUCT_URL = "https://store-reload.onrender.com/api/v1/admin/product";
+// export const PRODUCT_URL = "http://localhost:5000/api/v1/admin/product";
 
-// export const CATEGORY_URL =
-//   "https://store-reload.onrender.com/api/v1/admin/category";
+export const CATEGORY_URL = "https://store-reload.onrender.com/api/v1/admin/category";
 
-export const CATEGORY_URL = "http://localhost:5000/api/v1/admin/category";
+// export const CATEGORY_URL = "http://localhost:5000/api/v1/admin/category";
