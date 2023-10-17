5c72cb61e2a235ce706b796b3f8693162d753563
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 21:22:10 +0100

    formated code with prettier

--- src/hooks/useAuthToken.ts
+++ src/hooks/useAuthToken.ts
@@ -1,7 +1,7 @@
 export const useAuthToken = () => {
-  const token = window.localStorage.getItem("token") as string;
-  if (window.localStorage.getItem("token")) {
-    return { token };
-  }
-  return { token: null };
+     const token = window.localStorage.getItem("token") as string;
+     if (window.localStorage.getItem("token")) {
+          return { token };
+     }
+     return { token: null };
 };
