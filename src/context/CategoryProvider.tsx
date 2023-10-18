440e46b1c7036876ec788db250082fd740de954f
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-18 09:10:14 +0100

    formatted category and product providers with prettier

--- src/context/CategoryProvider.tsx
+++ src/context/CategoryProvider.tsx
@@ -4,59 +4,63 @@ import { api } from "../api/request";
 import { useAuthToken } from "../hooks/useAuthToken";
 
 interface ICategory {
-    _id: string;
-    name: string;
-    productType: string;
+     product_type: string;
+     _id: string;
+     name: string;
 }
 
 interface IContextType {
-    categories: ICategory[];
-    getCategories: () => Promise<void>;
-    setIsCategoryModal: React.Dispatch<React.SetStateAction<boolean>>;
-    isCategoryModal: boolean;
+     categories: ICategory[];
+     getCategories: () => Promise<void>;
+     setIsCategoryModal: React.Dispatch<React.SetStateAction<boolean>>;
+     isCategoryModal: boolean;
 }
 interface IProps {
-    children: React.ReactNode;
+     children: React.ReactNode;
 }
 
 const CategoryContext = React.createContext<IContextType>({
-    categories: [{ _id: "1", name: "Beverages", productType: "medical" }],
-    getCategories: async () => {},
-    isCategoryModal: false,
-    setIsCategoryModal: () => {},
+     categories: [{ _id: "1", name: "Beverages", product_type: "medical" }],
+     getCategories: async () => {},
+     isCategoryModal: false,
+     setIsCategoryModal: () => {},
 });
 
 export const CategoryProvider = ({ children }: IProps) => {
-    const { token } = useAuthToken();
-    const [categories, setCategories] = useState<ICategory[]>([]);
-    const [isCategoryModal, setIsCategoryModal] = useState<boolean>(false);
+     const { token } = useAuthToken();
+     const [categories, setCategories] = useState<ICategory[]>([]);
+     const [isCategoryModal, setIsCategoryModal] = useState<boolean>(false);
 
-    const getCategories = useCallback(async () => {
-        try {
-            const res = await api().get(CATEGORY_URL + "/", {
-                headers: {
-                    Authorization: "Bearer " + token,
-                },
-            });
-            setCategories(res.data.data);
-        } catch (error: any) {
-            console.log("error:", error);
-        }
-    }, []);
+     const getCategories = useCallback(async () => {
+          try {
+               const res = await api().get(CATEGORY_URL, {
+                    headers: {
+                         Authorization: "Bearer " + token,
+                    },
+               });
+               setCategories(res.data.data);
+          } catch (error: any) {
+               console.log("error:", error);
+          }
+     }, []);
 
-    useEffect(() => {
-        getCategories();
-    }, [getCategories]);
+     useEffect(() => {
+          getCategories();
+     }, [getCategories]);
 
-    const dataObj = {
-        categories,
-        getCategories,
-        isCategoryModal,
-        setIsCategoryModal,
-    };
-    return <CategoryContext.Provider value={dataObj}>{children}</CategoryContext.Provider>;
+     const dataObj = {
+          categories,
+          getCategories,
+          isCategoryModal,
+          setIsCategoryModal,
+     };
+     return (
+          <CategoryContext.Provider value={dataObj}>
+               {children}
+          </CategoryContext.Provider>
+     );
 };
 
 export const useCategoryData = () => {
-    return useContext(CategoryContext);
+     return useContext(CategoryContext);
 };
--- src/context/ProductProvider.tsx
+++ src/context/ProductProvider.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
+import React, {
+     createContext,
+     useCallback,
+     useContext,
+     useEffect,
+     useState,
+} from "react";
 import { api } from "../api/request";
 import { useAuthToken } from "../hooks/useAuthToken";
 import { useNavigate } from "react-router-dom";
@@ -6,72 +12,76 @@ import { IProduct } from "../components/Products/types";
 import { PRODUCT_URL } from "../constants";
 
 interface IContext {
-    products: IProduct[];
-    loading: boolean;
-    productLength: number;
-    type: "general" | "medical";
-    setType: React.Dispatch<React.SetStateAction<"general" | "medical">>;
-    setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
+     products: IProduct[];
+     loading: boolean;
+     productLength: number;
+     type: "general" | "medical";
+     setType: React.Dispatch<React.SetStateAction<"general" | "medical">>;
+     setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ProductContext = createContext<IContext>({
-    products: [],
-    loading: false,
-    productLength: 0,
-    type: "general",
-    setType: () => {},
-    setRefresh: () => {},
+     products: [],
+     loading: false,
+     productLength: 0,
+     type: "general",
+     setType: () => {},
+     setRefresh: () => {},
 });
 
 interface IProductProvider {
-    children: React.ReactNode;
+     children: React.ReactNode;
 }
 
 export const ProductProvider: React.FC<IProductProvider> = ({ children }) => {
-    const { token } = useAuthToken();
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState<boolean>(false);
-    const [refresh, setRefresh] = useState<boolean>(false);
-    const [productLength, setProductLength] = useState<number>(0);
-    const [products, setProducts] = useState<IProduct[]>([]);
-    const [type, setType] = useState<"general" | "medical">("general");
+     const { token } = useAuthToken();
+     const navigate = useNavigate();
+     const [loading, setLoading] = useState<boolean>(false);
+     const [refresh, setRefresh] = useState<boolean>(false);
+     const [productLength, setProductLength] = useState<number>(0);
+     const [products, setProducts] = useState<IProduct[]>([]);
+     const [type, setType] = useState<"general" | "medical">("general");
 
-    const fetchProductData = useCallback(async () => {
-        try {
-            setLoading(true); // Set loading to true when starting the request
-            const result = await api().get(`${PRODUCT_URL}/?type=${type}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+     const fetchProductData = useCallback(async () => {
+          try {
+               setLoading(true); // Set loading to true when starting the request
+               const result = await api().get(`${PRODUCT_URL}/?type=${type}`, {
+                    headers: {
+                         Authorization: `Bearer ${token}`,
+                    },
+               });
 
-            setLoading(false); // Set loading to false when the request is complete
-            setProductLength(result?.data.data.length);
-            setProducts(result?.data.data);
-        } catch (error: any) {
-            setLoading(false); // Set loading to false in case of an error
-            console.error("ERROR: ", error);
-        }
-    }, [type, navigate]);
+               setLoading(false); // Set loading to false when the request is complete
+               setProductLength(result?.data.data.length);
+               setProducts(result?.data.data);
+          } catch (error: any) {
+               setLoading(false); // Set loading to false in case of an error
+               console.error("ERROR: ", error);
+          }
+     }, [type, navigate]);
 
-    useEffect(() => {
-        if (refresh) setRefresh(false);
+     useEffect(() => {
+          if (refresh) setRefresh(false);
 
-        fetchProductData();
-    }, [refresh, fetchProductData]);
+          fetchProductData();
+     }, [refresh, fetchProductData]);
 
-    const values: IContext = {
-        products,
-        loading,
-        productLength,
-        type,
-        setType,
-        setRefresh,
-    };
+     const values: IContext = {
+          products,
+          loading,
+          productLength,
+          type,
+          setType,
+          setRefresh,
+     };
 
-    return <ProductContext.Provider value={values}>{children}</ProductContext.Provider>;
+     return (
+          <ProductContext.Provider value={values}>
+               {children}
+          </ProductContext.Provider>
+     );
 };
 
 export const useProductData = () => {
-    return useContext(ProductContext);
+     return useContext(ProductContext);
 };
