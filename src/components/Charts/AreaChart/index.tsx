f0134eaf045448b8d0e72fba014d8af61c810ec8
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-18 19:55:25 +0100

    added area chart with dommie data

--- src/components/Charts/AreaChart/index.tsx
+++ src/components/Charts/AreaChart/index.tsx
@@ -1,60 +1,61 @@
-// import React from "react";
-// import ReactApexChart from "react-apexcharts";
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 // import { series } from "./your-series-data"; // Import your series data
 
-// export const AreaChart: React.FC = () => {
-//      const state = {
-//           series: [
-//                {
-//                     name: "STOCK ABC",
-//                     data: series.monthDataSeries1.prices,
-//                },
-//           ],
-//           options: {
-//                chart: {
-//                     type: "area",
-//                     height: 350,
-//                     zoom: {
-//                          enabled: false,
-//                     },
-//                },
-//                dataLabels: {
-//                     enabled: false,
-//                },
-//                stroke: {
-//                     curve: "straight",
-//                },
-//                title: {
-//                     text: "Fundamental Analysis of Stocks",
-//                     align: "left",
-//                },
-//                subtitle: {
-//                     text: "Price Movements",
-//                     align: "left",
-//                },
-//                labels: series.monthDataSeries1.dates,
-//                xaxis: {
-//                     type: "datetime",
-//                },
-//                yaxis: {
-//                     opposite: true,
-//                },
-//                legend: {
-//                     horizontalAlign: "left",
-//                },
-//           },
-//      };
+export const AreaChart: React.FC = () => {
+     const state = {
+          series: [
+               {
+                    name: "STOCK ABC",
+                    data: [30, 40, 35, 125, 49, 60, 125],
+               },
+          ],
+          options: {
+               chart: {
+                    type: "area",
+                    height: 350,
+                    zoom: {
+                         enabled: false,
+                    },
+               },
+               dataLabels: {
+                    enabled: false,
+               },
+               stroke: {
+                    curve: "straight",
+               },
+               title: {
+                    text: "Fundamental Analysis of Stocks",
+                    align: "left",
+               },
+               subtitle: {
+                    text: "Price Movements",
+                    align: "left",
+               },
+               labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+               xaxis: {
+                    type: "day",
+               },
+               yaxis: {
+                    opposite: true,
+               },
+               legend: {
+                    horizontalAlign: "left",
+               },
+          },
+     };
 
-//      return (
-//           <div id="chart">
-//                <ReactApexChart
-//                     options={state.options as any}
-//                     series={state.series}
-//                     type="area"
-//                     height={350}
-//                />
-//           </div>
-//      );
-// };
+     return (
+          <div id="chart">
+               <ReactApexChart
+                    options={state.options as any}
+                    series={state.series}
+                    type="area"
+                    height={350}
+                    width={"100%"}
+               />
+          </div>
+     );
+};
 
-// export default ApexChart;
+export default AreaChart;
