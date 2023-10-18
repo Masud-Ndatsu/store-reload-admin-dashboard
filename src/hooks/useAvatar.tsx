0852f03aa6ef0ff7d98c2aaa7215682c113889ee
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-18 09:08:28 +0100

    formatted code with prettier

--- src/hooks/useAvatar.tsx
+++ src/hooks/useAvatar.tsx
@@ -4,26 +4,26 @@ import { api } from "../api/request";
 import { useAuthToken } from "./useAuthToken";
 
 export const useAvatar = () => {
-  const { token } = useAuthToken();
-  const [avatar, setAvatar] = useState<string>("");
+     const { token } = useAuthToken();
+     const [avatar, setAvatar] = useState<string>("");
 
-  const getAvatar = useCallback(
-    async function () {
-      if (!token) return;
-      try {
-        const res = await api().get(`${USER_URL}/avatar`, {
-          headers: { Authorization: "Bearer " + token },
-        });
-        setAvatar(res.data.data);
-      } catch (error: any) {
-        console.log({ error });
-      }
-    },
-    [token]
-  );
+     const getAvatar = useCallback(
+          async function () {
+               if (!token) return;
+               try {
+                    const res = await api().get(`${USER_URL}/avatar`, {
+                         headers: { Authorization: "Bearer " + token },
+                    });
+                    setAvatar(res.data.data);
+               } catch (error: any) {
+                    console.log({ error });
+               }
+          },
+          [token]
+     );
 
-  useEffect(() => {
-    getAvatar();
-  }, [getAvatar]);
-  return { avatar };
+     useEffect(() => {
+          getAvatar();
+     }, [getAvatar]);
+     return { avatar };
 };
