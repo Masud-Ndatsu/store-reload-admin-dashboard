0c31a7bf9a5f582619c8681667f4df1255d5a2eb
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-18 10:02:24 +0100

    modified products component

--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -9,90 +9,97 @@ import { Pagination } from "../Pagination";
 import { CategoryForm } from "../CategoryForm";
 
 export const Product = () => {
-    const { type, setType, productLength, loading } = useProductData();
-    const { setIsCategoryModal } = useCategoryData();
-    const [showModal, setShowModal] = useState<boolean>(false);
+     const { type, setType, productLength, loading } = useProductData();
+     const { setIsCategoryModal } = useCategoryData();
+     const [showModal, setShowModal] = useState<boolean>(false);
 
-    return (
-        <div>
-            <div className="dash-bar" style={{ marginBottom: "1rem" }}>
-                <h2>Dashboard</h2>
-                {productLength > 0 && (
-                    <button className="dash-button" onClick={() => setShowModal(true)}>
-                        <IoIosAdd />
-                        Add product
-                    </button>
-                )}
-            </div>
-            <div>
-                <div
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                    }}
-                >
+     return (
+          <div>
+               <div className="dash-bar" style={{ marginBottom: "1rem" }}>
+                    <h2>Dashboard</h2>
+                    {productLength > 0 && (
+                         <button
+                              className="dash-button"
+                              onClick={() => setShowModal(true)}
+                         >
+                              <IoIosAdd />
+                              Add product
+                         </button>
+                    )}
+               </div>
+               <div>
                     <div
-                        className="tabs"
-                        style={{
-                            display: "flex",
-                            gap: "2rem",
-                            backgroundColor: "#f2f2f2",
-                            maxWidth: "max-content",
-                            padding: "0.5rem 0.75rem",
-                            borderRadius: "20px",
-                        }}
+                         style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "space-between",
+                         }}
                     >
-                        <button
-                            id="general"
-                            style={{
-                                backgroundColor: `${type === "general" ? "white" : ""}`,
-                            }}
-                            onClick={() => {
-                                setType("general");
-                            }}
-                        >
-                            Consumable goods
-                        </button>
-                        <button
-                            id="medical"
-                            style={{
-                                backgroundColor: `${type === "medical" ? "white" : ""}`,
-                            }}
-                            onClick={() => {
-                                setType("medical");
-                            }}
-                        >
-                            Medicals
-                        </button>
+                         <div
+                              className="tabs"
+                              style={{
+                                   display: "flex",
+                                   gap: "2rem",
+                                   backgroundColor: "#f2f2f2",
+                                   maxWidth: "max-content",
+                                   padding: "0.5rem 0.75rem",
+                                   borderRadius: "20px",
+                              }}
+                         >
+                              <button
+                                   id="general"
+                                   style={{
+                                        backgroundColor: `${
+                                             type === "general" ? "white" : ""
+                                        }`,
+                                   }}
+                                   onClick={() => {
+                                        setType("general");
+                                   }}
+                              >
+                                   Consumable goods
+                              </button>
+                              <button
+                                   id="medical"
+                                   style={{
+                                        backgroundColor: `${
+                                             type === "medical" ? "white" : ""
+                                        }`,
+                                   }}
+                                   onClick={() => {
+                                        setType("medical");
+                                   }}
+                              >
+                                   Medicals
+                              </button>
+                         </div>
+                         <Pagination loading={loading} />
                     </div>
-                    <Pagination loading={loading} />
-                </div>
-                <div
-                    style={{
-                        margin: "1rem 0",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "2rem",
-                    }}
-                >
-                    <span
-                        style={{
-                            display: "block",
-                            backgroundColor: "var(--main-blue)",
-                            color: "white",
-                            padding: ".65rem 1rem",
-                            borderRadius: "5px",
-                            fontSize: ".75rem",
-                            cursor: "pointer",
-                            letterSpacing: "1px",
-                        }}
-                        className="add-category"
-                        onClick={() => setIsCategoryModal(true)}
+                    <div
+                         style={{
+                              margin: "1rem 0",
+                              display: "flex",
+                              alignItems: "center",
+                              gap: "2rem",
+                         }}
                     >
-                        Add product category
-                    </span>
-                    {/* <div style={{ display: "flex", gap: "1rem" }}>
+                         <span
+                              style={{
+                                   display: "block",
+                                   backgroundColor: "var(--main-blue)",
+                                   color: "white",
+                                   padding: ".65rem 1rem",
+                                   borderRadius: "5px",
+                                   fontSize: ".75rem",
+                                   cursor: "pointer",
+                                   letterSpacing: "1px",
+                              }}
+                              className="add-category"
+                              onClick={() => setIsCategoryModal(true)}
+                         >
+                              Add product category
+                         </span>
+                         {/* <div style={{ display: "flex", gap: "1rem" }}>
                         {categories.map((categoryObj) => (
                             <button
                                 key={categoryObj._id}
@@ -111,29 +118,40 @@ export const Product = () => {
                             </button>
                         ))}
                     </div> */}
-                </div>
-                {loading ? (
-                    <Loading />
-                ) : !productLength ? (
-                    <>
-                        <div className="main-area">
-                            <div style={{ textAlign: "center" }}>
-                                <p style={{ letterSpacing: "1.2px", marginBottom: "1.2rem" }}>
-                                    You have not added any product. Start adding.
-                                </p>
-                                <button onClick={() => setShowModal(true)}>
-                                    <IoIosAdd />
-                                    Add product
-                                </button>
-                            </div>
-                        </div>
-                    </>
-                ) : (
-                    <ProductList />
-                )}
-                <ProductForm showModal={showModal} setShowModal={setShowModal} />
-                <CategoryForm />
-            </div>
-        </div>
-    );
+                    </div>
+                    {loading ? (
+                         <Loading />
+                    ) : !productLength ? (
+                         <>
+                              <div className="main-area">
+                                   <div style={{ textAlign: "center" }}>
+                                        <p
+                                             style={{
+                                                  letterSpacing: "1.2px",
+                                                  marginBottom: "1.2rem",
+                                             }}
+                                        >
+                                             You have not added any product.
+                                             Start adding.
+                                        </p>
+                                        <button
+                                             onClick={() => setShowModal(true)}
+                                        >
+                                             <IoIosAdd />
+                                             Add product
+                                        </button>
+                                   </div>
+                              </div>
+                         </>
+                    ) : (
+                         <ProductList />
+                    )}
+                    <ProductForm
+                         showModal={showModal}
+                         setShowModal={setShowModal}
+                    />
+                    <CategoryForm />
+               </div>
+          </div>
+     );
 };
--- src/components/Products/ProductItem.tsx
+++ src/components/Products/ProductItem.tsx
@@ -9,64 +9,66 @@ import { PRODUCT_URL } from "../../constants";
 import "./style.css";
 
 type Props = {
-    product: IProduct;
-    index: number;
+     product: IProduct;
+     index: number;
 };
 
 export const ProductItem = ({ product, index }: Props) => {
-    const { token } = useAuthToken();
-    const { setRefresh } = useProductData();
-    const { _id: id } = product;
-    const handleDelete = useCallback(async (): Promise<void> => {
-        try {
-            const result = await api().delete(
-                PRODUCT_URL + "/" + id,
+     const { token } = useAuthToken();
+     const { setRefresh } = useProductData();
+     const { _id: id } = product;
+     const handleDelete = useCallback(async (): Promise<void> => {
+          try {
+               const result = await api().delete(
+                    PRODUCT_URL + "/" + id,
 
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-            if (result.status == 200) {
-                setRefresh(true);
-            }
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (error: any) {
-            console.log("ERROR", error.data);
-        }
-    }, [id]);
+                    {
+                         headers: {
+                              Authorization: `Bearer ${token}`,
+                         },
+                    }
+               );
+               if (result.status == 200) {
+                    setRefresh(true);
+               }
+               // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          } catch (error: any) {
+               console.log("ERROR", error.data);
+          }
+     }, [id]);
 
-    return (
-        <>
-            <tr className="product-item" style={{ cursor: "pointer" }}>
-                <td>{(index + 1).toString().padStart(2, "0")}</td>
-                <td>
-                    <img
-                        style={{
-                            width: "100%",
-                            maxWidth: "50px",
-                            maxHeight: "50px",
-                            height: "100%",
-                            margin: "auto",
-                        }}
-                        src={product.images[0]}
-                        alt=""
-                    />
-                </td>
-                <td>{product.name}</td>
-                <td>{product.size ?? "Carton"}</td>
-                <td>{product.description}</td>
-                <td>{product.price ?? 500}</td>
-                <td>
-                    <span onClick={handleDelete}>
-                        <RiDeleteBin6Line />
-                    </span>
-                    <span>
-                        <GrEdit style={{ marginLeft: "20px" }} />
-                    </span>
-                </td>
-            </tr>
-        </>
-    );
+     return (
+          <>
+               <tr className="product-item" style={{ cursor: "pointer" }}>
+                    <td>{(index + 1).toString().padStart(2, "0")}</td>
+                    <td>
+                         <img
+                              style={{
+                                   width: "100%",
+                                   maxWidth: "50px",
+                                   maxHeight: "50px",
+                                   height: "100%",
+                                   margin: "auto",
+                                   objectFit: "cover",
+                              }}
+                              loading="lazy"
+                              src={product.images[0]}
+                              alt=""
+                         />
+                    </td>
+                    <td>{product.name}</td>
+                    <td>{product.size ?? "Carton"}</td>
+                    <td>{product.description}</td>
+                    <td>{product.price ?? 500}</td>
+                    <td>
+                         <span onClick={handleDelete}>
+                              <RiDeleteBin6Line />
+                         </span>
+                         <span>
+                              <GrEdit style={{ marginLeft: "20px" }} />
+                         </span>
+                    </td>
+               </tr>
+          </>
+     );
 };
